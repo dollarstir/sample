2bbd769ac575ad190d333cb96c2891b3b6652baa
dollarstir <kpin463@gmail.com>
2022-03-20 22:54:35 +0000

    new

--- booking_edit.php
+++ booking_edit.php
@@ -33,4 +33,5 @@ $checker->mainchecker('/tms/login');
 	                                                                                                    <link href="/tms/assets/css/basic-ui/custom_sweetalert.css" rel="stylesheet" type="text/css" />
 	                                                                                                        <!-- <link href="assets/css/forms/selectize.min.css" rel="stylesheet" type="text/css"> -->
 	                                                                                                            <!-- Page Level Plugin/Style Ends -->
-	                                                                                                            
+	                                                                                                        </head>head>
+	                                                                                                        
