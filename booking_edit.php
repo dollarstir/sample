b2ef97654652afb5c99909cbc623126b4a4f7a51
dollarstir <kpin463@gmail.com>
2022-03-20 22:50:43 +0000

    new

--- booking_edit.php
+++ booking_edit.php
@@ -14,4 +14,5 @@ $checker->mainchecker('/tms/login');
 	                        <link href="/tms/assets/css/loader.css" rel="stylesheet" type="text/css" />
 	                            <script src="/tms/assets/js/loader.js"></script>script>
 	                                <!-- Common Styles Starts -->
-	                                
+	                                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+	                                    
