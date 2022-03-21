c8858eba5bcd7d28bd7e5d10a561fa85c4395496
dollarstir <kpin463@gmail.com>
2022-03-21 05:33:04 +0000

    new

--- booking_edit.php
+++ booking_edit.php
@@ -10,4 +10,5 @@ $checker->mainchecker('/tms/login');
 	            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
 	                <title>TUCEE - Add Bookings </title>title>
 	                    <link rel="icon" type="image/x-icon" href="common-assets/img/favicon.ico"/>
-	                    
+	                        <link href="assets/css/loader.css" rel="stylesheet" type="text/css" />
+	                        
