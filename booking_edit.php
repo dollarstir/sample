15545636a6f05e32a5523799e6e39e92eb08f8c6
dollarstir <kpin463@gmail.com>
2022-03-22 13:38:12 +0000

    new

--- booking_edit.php
+++ booking_edit.php
@@ -23,6 +23,7 @@
 	        	                	        if (!('theme' in localStorage)) {
 	        	                	        	            localStorage.theme = 'system';
 	        	                	        	                    }
+
 	        	                	        	                    
 	        	                	        }
 	        	                }
