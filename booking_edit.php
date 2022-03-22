bb80b3005bf9291a513ab783f563207d3ca4144b
dollarstir <kpin463@gmail.com>
2022-03-22 13:36:47 +0000

    new

--- booking_edit.php
+++ booking_edit.php
@@ -15,7 +15,8 @@
 	        	            	        	            	                document.documentElement.classList.add('dark');
 	        	            	        	            	                            } else {
 	        	            	        	            	                            	                document.documentElement.classList.remove('dark');
-	        	            	        	            	                            	                
+	        	            	        	            	                            	                            }
+	        	            	        	            	                            	                            
 	        	            	        	            	                            }
 	        	            	        	            }
 	        	            	        }
