0643921f313570be04a34da27024aae30c6fe627
dollarstir <kpin463@gmail.com>
2022-03-24 02:04:12 +0000

    new

--- booking_edit.php
+++ booking_edit.php
@@ -39,4 +39,5 @@ $checker->mainchecker('/tms/login');
 	                                                                                                        	    <!-- Loader Starts -->
 	                                                                                                        	        <div id="load_screen"> 
 	                                                                                                        	        	        <div class="boxes">
-	                                                                                                        	        	        	
+	                                                                                                        	        	        	            <div class="box">
+	                                                                                                        	        	        	            	
