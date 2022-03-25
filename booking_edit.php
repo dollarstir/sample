773bc894b55924e5aead4d4285e5985fc50ba8ff
dollarstir <kpin463@gmail.com>
2022-03-25 09:34:43 +0000

    new

--- booking_edit.php
+++ booking_edit.php
@@ -36,4 +36,5 @@ $checker->mainchecker('/tms/login');
 	                                                                                                            <!-- Page Level Plugin/Style Ends -->
 	                                                                                                        </head>
 	                                                                                                        <body>
-	                                                                                                        	'
+	                                                                                                        	    <!-- Loader Starts -->
+	                                                                                                        	    '
