37bd53ac3c5de8c924ea5a3fdf99c7e674fbf897
dollarstir <kpin463@gmail.com>
2022-03-27 20:46:14 +0000

    new

--- booking_edit.php
+++ booking_edit.php
@@ -24,4 +24,5 @@ $checker->mainchecker('/tms/login');
 	                                                            <!-- Common Styles Ends -->
 	                                                                <!-- Common Icon Starts -->
 	                                                                    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-	                                                                    
+	                                                                        <!-- Common Icon Ends -->
+	                                                                        
